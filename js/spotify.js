add7be00b3b8088d155896da5d156e2ac9d68044
Arvindram Krishnamoorthy <karvi90@yahoo.com>
2015-11-08 20:33:14 -0800

    New

--- js/spotify.js
+++ js/spotify.js
@@ -3,26 +3,45 @@ var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
 var myApp = angular.module('myApp', [])
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
-  $scope.audioObject = {}
-  $scope.getSongs = function() {
-    $http.get(baseUrl + $scope.track).success(function(response){
-      data = $scope.tracks = response.tracks.items
+
+    var highest = 0;
+    var check = 0;
+
+    $scope.audioObject = {}
+    $scope.getSongs = function() {
+        $http.get(baseUrl + $scope.track).success(function(response){
+            data = $scope.tracks = response.tracks.items;
+        }).then(function(){
+            for (var i = 0; i < data.length; i++) {
+                check = data[i]["popularity"];
+                if (check > highest) {
+                    highest = check;
+                }
+            } 
+            console.log('popularity: ' + highest);
+        });
+    }
+
+
+    $scope.play = function(song) {
+        if($scope.currentSong == song) {
+            $scope.audioObject.pause()
+            $scope.currentSong = false
+            return
+        }
+        else {
+            if($scope.audioObject.pause != undefined) $scope.audioObject.pause()
+            $scope.audioObject = new Audio(song);
+            $scope.audioObject.play()  
+            $scope.currentSong = song
+        }
+    }
       
-    })
-  }
-  $scope.play = function(song) {
-    if($scope.currentSong == song) {
-      $scope.audioObject.pause()
-      $scope.currentSong = false
-      return
+    $scope.popCheck = function(song) {
+        
+
+        
     }
-    else {
-      if($scope.audioObject.pause != undefined) $scope.audioObject.pause()
-      $scope.audioObject = new Audio(song);
-      $scope.audioObject.play()  
-      $scope.currentSong = song
-    }
-  }
 })
 
 // Add tool tips to anything with a title property
