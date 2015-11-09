0553b17a39cf1018cb40878249d8047db4d22a71
Arvindram Krishnamoorthy <karvi90@yahoo.com>
2015-11-08 23:36:02 -0800

    Finalized

--- js/spotify.js
+++ js/spotify.js
@@ -1,13 +1,19 @@
 var data;
 var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
+
+//Initializes the app to utilize Angular
 var myApp = angular.module('myApp', [])
 
+//Initializes controller, takes in scope and http as parameters 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
 
     var highest = 0;
     var check = 0;
 
     $scope.audioObject = {}
+
+    //Performs Ajax request to return an array of objects with name property containing the user-inputted track
+    //then finds highest number; this quantifies the popularity of the most popular song. 
     $scope.getSongs = function() {
         $http.get(baseUrl + $scope.track).success(function(response){
             data = $scope.tracks = response.tracks.items;
@@ -18,11 +24,10 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
                     highest = check;
                 }
             } 
-            console.log('popularity: ' + highest);
         });
     }
 
-
+    //Samples the track.
     $scope.play = function(song) {
         if($scope.currentSong == song) {
             $scope.audioObject.pause()
@@ -36,15 +41,18 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
             $scope.currentSong = song
         }
     }
-      
+
+    //Checks whether the selected song is the most popular. If the user won, warns of page reload,
+    //else, prompts user to continue guessing.  
     $scope.popCheck = function(song, popularity) {
-        console.log(popularity);
         if(popularity == highest) {
-            alert('YOU WIN!');
+            alert("YOU WIN\nPlease stand-by for a page refresh");
+            document.location.reload(true); 
         } else {
             alert('Keep trying.');
         }  
     }
+
 })
 
 // Add tool tips to anything with a title property
